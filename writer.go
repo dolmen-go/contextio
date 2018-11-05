38b007447a63a4aa0179aa54ea9a29aa27d41645
Olivier Mengué <dolmen@cpan.org>
2018-11-05 22:17:51 +0100

    Initial minimalist writer

---
+++ writer.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2018 Olivier Mengué
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package ctxwriter provides a Writer that stops accepting data when an attached context is canceled.
+package ctxwriter
+
+import (
+	"context"
+	"io"
+)
+
+type writer struct {
+	ctx context.Context
+	w   io.Writer
+}
+
+// New wraps an io.Writer to handle context cancellation.
+//
+// Context state is checked BEFORE any Write.
+func New(ctx context.Context, w io.Writer) io.Writer {
+	return &writer{ctx: ctx, w: w}
+}
+
+// Write implements io.Writer, but with context awareness.
+func (w *writer) Write(p []byte) (n int, err error) {
+	select {
+	case <-w.ctx.Done():
+		return 0, w.ctx.Err()
+	default:
+		return w.w.Write(p)
+	}
+}
