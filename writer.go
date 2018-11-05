04d216c9eb0045d2ba3a73ed825ee64c4b77f182
Olivier Mengué <dolmen@cpan.org>
2018-11-05 23:01:47 +0100

    Implement io.ReadFrom to allow io.Copy to select the best strategy

--- writer.go
+++ writer.go
@@ -27,19 +27,58 @@ type writer struct {
 	w   io.Writer
 }
 
+type copier struct {
+	writer
+}
+
 // New wraps an io.Writer to handle context cancellation.
 //
 // Context state is checked BEFORE any Write.
 func New(ctx context.Context, w io.Writer) io.Writer {
-	return &writer{ctx: ctx, w: w}
+	return &copier{writer{ctx: ctx, w: w}}
 }
 
 // Write implements io.Writer, but with context awareness.
+//
+// The returned Writer also implements io.ReadFrom to allow io.Copy to select the best strategy
+// while still checking the context state before every chunk transfer.
 func (w *writer) Write(p []byte) (n int, err error) {
 	select {
 	case <-w.ctx.Done():
 		return 0, w.ctx.Err()
 	default:
 		return w.w.Write(p)
+	}
+}
+
+type reader struct {
+	ctx context.Context
+	r   io.Reader
+}
+
+func (r *reader) Read(p []byte) (n int, err error) {
+	select {
+	case <-r.ctx.Done():
+		return 0, r.ctx.Err()
+	default:
+		return r.r.Read(p)
+	}
+}
+
+// ReadFrom implements interface io.ReaderFrom, but with context awareness.
+//
+// This should allow efficient copying allowing writer or reader to define the chunk size.
+func (w *copier) ReadFrom(r io.Reader) (n int64, err error) {
+	if _, ok := w.w.(io.ReaderFrom); ok {
+		// Let the original Writer decide the chunk size.
+		return io.Copy(w.writer.w, &reader{ctx: w.ctx, r: r})
+	}
+	select {
+	case <-w.ctx.Done():
+		return 0, w.ctx.Err()
+	default:
+		// The original Writer is not a ReaderFrom.
+		// Let the Reader decide the chunk size.
+		return io.Copy(&w.writer, r)
 	}
 }
